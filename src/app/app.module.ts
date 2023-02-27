2cc44735f69119034a5e4a939355a439c8710f39
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-02-27 17:00:45 +0100

    Creación de componentes (header, home, user, newuser, updatebuser y footer)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { NewuserComponent } from './components/newuser/newuser.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UpdateuserComponent } from './components/user/updateuser/updateuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    UserComponent,
+    NewuserComponent,
+    FooterComponent,
+    UpdateuserComponent
   ],
   imports: [
     BrowserModule,
