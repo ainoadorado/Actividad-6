f2cf8de2eb3d755f1703f562c05f419f3b98ec85
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-02-27 17:54:45 +0100

    Definición servicio Users

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { USERS } from '../db/users.db';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
+  private arrUsers: User[] = USERS;
+
   constructor() { }
+
+  getAll(): User[] {
+    return this.arrUsers;
+  }
 }
