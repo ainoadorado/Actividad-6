964f8e3572d25eeead954ce81cfb09998dffabd2
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-02-27 20:21:14 +0100

    Creación de las cards de los users en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  arrUsers: User[] = [];
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.arrUsers = this.usersService.getAll()
+  }
 
 }
