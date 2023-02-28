db44b0a47994cc833debe65aee1fe5bde43292e3
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-02-28 03:54:45 +0100

    Creación de updateuser capturando ruta dinámica

--- src/app/components/user/updateuser/updateuser.component.ts
+++ src/app/components/user/updateuser/updateuser.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-updateuser',
   templateUrl: './updateuser.component.html',
   styleUrls: ['./updateuser.component.css']
 })
-export class UpdateuserComponent {
+export class UpdateuserComponent implements OnInit {
+  @Input() myUser: User | any;
   updateUser: FormGroup;
 
-  constructor() {
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private usersService: UsersService) {
+
     this.updateUser = new FormGroup({
       first_name: new FormControl("", [
         Validators.required,
@@ -29,6 +36,16 @@ export class UpdateuserComponent {
       ])
     })
   }
+
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe((params: any) => {
+      let id = params.url;
+      this.myUser = this.usersService.getById(id);
+      this.updateUser.patchValue({ first_name: this.myUser.first_name, last_name: this.myUser.last_name, email: this.myUser.email, image: this.myUser.image });
+    })
+  }
+
+
 
   getDataForm() {
     console.log(this.updateUser.value)
